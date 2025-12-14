43c34daaccfe5ded841ad42a95ae0c8617259bbc
Sahar <saharr449@gmail.com>
2025-12-14 16:18:42 +0200

    added specific version to eks

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -30,6 +30,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
+  version = "1.33"
   vpc_config {
     subnet_ids = var.private_subnet_ids
   }
